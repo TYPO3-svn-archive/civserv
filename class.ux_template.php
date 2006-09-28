5783f615ad83b74144fd6b950d9097f858c1ed86
msbritta <msbritta@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-09-28 09:52:48 +0000

    hack for withdrawing the right to create new versions of pages from all be_users. to_do: check if this interferes with ordinary typo3 pages
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/civserv/trunk@3791 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ class.ux_template.php
@@ -0,0 +1,24 @@
+<?php
+
+/** 
+* User-Extension of template class.
+*
+* @author    Britta Kohorst <kohorst@citeq.de>
+*
+* this class extends typo3 core class with layout/output function for TYPO3 Backend Scripts
+*
+* aim: suppress "new version of page" - button for all be_users!
+* for the virtual townhall osiris, its all about non-pages records 
+* and no matter what rights the be_user has, we don't allow new versions of pages (which would 
+* turn out to be new versions of sysfolders storing civserv-records. that would make no sense)
+*
+*
+*/
+
+class ux_template extends template {    
+
+	function getVersionSelector($id,$noAction=FALSE){            
+		return '<br />';    
+	}
+}
+?>
