b2192cf14a0c74a513f3f0ade1bae3dba18a3771
msbritta <msbritta@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-10-18 12:39:40 +0000

    introduced messaging for BE - Forms (the editor gets a message informing him that he has to save a service-record, before he can relate it to a position-record)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/civserv/trunk@3932 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ res/class.tx_civserv_user_be_msg.php
@@ -0,0 +1,79 @@
+<?
+class tx_civserv_user_be_msg {
+	function user_TCAform_test($PA=array(), $fobj=array()) { //add array() bits to signator to enable calling this function with no parameters (happens through "displayCond" => "REC:NEW:true")
+		global $LANG;
+		$GLOBALS['TYPO3_DB']->debugOutput = TRUE;
+		$LANG->includeLLFile("EXT:civserv/res/locallang_user_be_msg.php");
+		
+		$table=$PA['table'];
+		$field=$PA['fieldConf']['dbField']; //dbField is a custom variable introduce in TCA to make the ll-fetching more flexible
+		debug($table, 'table');
+		debug($field, 'field');
+		debug($PA, '$PA');
+		debug($fobj, '$fobj'); 
+			 return '
+            <div style="
+                     border: 2px dashed #666666;
+                     width : 90%;
+                     margin: 5px 5px 5px 5px;
+                     padding: 5px 5px 5px 5px;"
+                     >
+				 <h2 style="color:red">'.$LANG->getLL("tx_civserv_user_be_msg.".$table.".".$field).'</h2>
+             </div>';
+	}
+	
+	function user_TCAform_sv_test($PA=array(), $fobj=array()) { //add array() bits to signator to enable calling this function with no parameters (happens through "displayCond" => "REC:NEW:true")
+		global $LANG;
+		$GLOBALS['TYPO3_DB']->debugOutput = TRUE;
+		$LANG->includeLLFile("EXT:civserv/res/locallang_user_be_msg.php");
+		
+		$table=$PA['table'];
+		$field=$PA['fieldConf']['dbField']; //dbField is a custom variable introduce in TCA to make the ll-fetching more flexible
+		debug($table, 'table');
+		debug($field, 'field');
+		debug($PA, '$PA');
+		debug($fobj, '$fobj'); 
+         return '
+            <div style="
+                     border: 2px dashed #666666;
+                     width : 90%;
+                     margin: 5px 5px 5px 5px;
+                     padding: 5px 5px 5px 5px;"
+                     >
+				 <h2 style="color:red">'.$LANG->getLL("tx_civserv_user_be_msg.".$table.".".$field).'</h2>
+             </div>';
+	}
+}
+
+//<h2 style="color:red">hasenschniepel</h2>
+
+//<h2 style="color:red">'.$LANG->getLL("tx_civserv_user_be_msg.tx_civserv_employee.em_position").'</h2>
+
+/*
+
+   1: class user_class {
+   2:     function user_TCAform_test($PA, $fobj)    {
+   3:         return '
+   4:             <div style="
+   5:                     border: 2px dashed #666666;
+   6:                     width : 90%;
+   7:                     margin: 5px 5px 5px 5px;
+   8:                     padding: 5px 5px 5px 5px;"
+   9:                     >
+  10:                 <h2>My Own Form Field:</h2>
+  11:                 <input
+  12:                     name="'.$PA['itemFormElName'].'"
+  13:                     value="'.htmlspecialchars($PA['itemFormElValue']).'"
+  14:                     onchange="'.htmlspecialchars(implode('',$PA['fieldChangeFunc'])).'"
+  15:                     '.$PA['onFocus'].'
+  16:                     />
+  17:             </div>';
+  18:     }
+  19: }
+
+*/
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/civserv/res/class.tx_civserv_user_class.php']) {
+	include_once ($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/civserv/res/class.tx_civserv_user_class.php']);
+}
+
+?>
---
+++ res/locallang_user_be_msg.php
@@ -0,0 +1,47 @@
+<?php
+/***************************************************************
+*  Copyright notice - www4.citeq.de: osiris_muenster special edition
+*
+*  (c) 2004 ProService (osiris@ercis.de)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Language labels for plugin 'civserv_pi1'
+ *
+ * This file is detected by the translation tool.
+ *
+ **/
+
+
+
+$LOCAL_LANG = Array (
+	"default" => Array (
+		"tx_civserv_user_be_msg.tx_civserv_employee.em_position" => "To add a position for this employee hit the 'save' button. Then you will find a Auswahlfeld with a wizard here",
+		"tx_civserv_user_be_msg.tx_civserv_service_sv_position_mm.sp_descr" => "The according service record is not published. You can only edit this record if the according service record is published!",
+		"tx_civserv_user_be_msg.tx_civserv_service.sv_similar_services" => "schnubbidiwupp",
+		"tx_civserv_user_be_msg.tx_civserv_service.sv_form" => "schnabbbidibap",
+	),
+	"de" => Array (
+		"tx_civserv_user_be_msg.tx_civserv_employee.em_position" => "Um dem Mitarbeiter eine Stelle zuzuweisen klicken Sie 'save'. Danach steht Ihnen hier eine Auswahlliste zur Verfügung",
+		"tx_civserv_user_be_msg.tx_civserv_service_sv_position_mm.sp_descr" => "Die Dienstleistung zu der dieser Mitarbeiter gehört ist nicht veröffentlicht! Dienstleistungs-Stellen-Zuordnungen können erst editiert werden, wenn der dazugehörige Dienstleistungsdatensatz veröffentlicht ist",	
+		"tx_civserv_user_be_msg.tx_civserv_service.sv_similar_services" => "schnubbidiwupp",
+		"tx_civserv_user_be_msg.tx_civserv_service.sv_form" => "schnabbbidibap",
+	),
+);
+?>
