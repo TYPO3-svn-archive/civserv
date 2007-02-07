3c3db3b48d2e7d9d391090c526ef91d8cdd21c34
msbritta <msbritta@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-02-07 08:06:33 +0000

    svn commit tests
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/civserv/trunk@4829 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.ux_template.php
+++ class.ux_template.php
@@ -12,7 +12,7 @@
 * and no matter what rights the be_user has, we don't allow new versions of pages (which would 
 * turn out to be new versions of sysfolders storing civserv-records. that would make no sense)
 *
-* raaah!
+* svn testing runs
 *
 */
 
