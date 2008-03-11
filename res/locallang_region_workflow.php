cbaa231a4c3a3db283b7d369e9cac775dc042b81
msbritta <msbritta@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-03-11 11:05:51 +0000

    started to re-work the email-workflow of regionalisation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/civserv/trunk@8535 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/locallang_region_workflow.php
+++ res/locallang_region_workflow.php
@@ -33,7 +33,7 @@
 $LOCAL_LANG = Array (
 	"default" => Array (
 		"xyz.emailmessage.subject" => "External Service: '###service_name###'",
-		"xyz.emailmessage.header" => "This E-Mail is automatically created by the O.S.I.R.I.S.-External-Service-Workflow! Please do NOT reply to this E-Mail!\n***********************************\n\nHello ###mandant_name###,\n",	
+		"xyz.emailmessage.header" => "This E-Mail is automatically created by the O.S.I.R.I.S.-External-Service-Workflow of ###sitename###! Please do NOT reply to this E-Mail!\n***********************************\n\nHello ###mandant_name###,\n",	
 		"xyz.emailmessage.service_new" => "The external service -- ###service_name### -- (donated by ###service_community###) has been edited.\n",	
 		"xyz.emailmessage.service_deleted" => "The external service -- ###service_name### -- (donated by ###service_community###) has been eleminated from your folder '###external_service_folder###'. This means the donator has either deleted the service or is no longer forwarding it to you.\n",	
 		"xyz.emailmessage.service_link" => "To view the service at the donator's klick the following link: ###service_link###\n\n",
@@ -41,7 +41,7 @@ $LOCAL_LANG = Array (
 	),
 	"de" => Array (
 		"xyz.emailmessage.subject" => "Externe Dienstleistung: '###service_name###'",
-		"xyz.emailmessage.header" => "Diese E-Mail wurde automatisch durch den O.S.I.R.I.S.-Externe-Dienstleistungen-Workflow generiert!\nBitte antworten Sie NICHT auf diese E-Mail!\n***********************************\n\nHallo ###mandant_name###,\n",	
+		"xyz.emailmessage.header" => "Diese E-Mail wurde automatisch durch den O.S.I.R.I.S.-Externe-Dienstleistungen-Workflow von ###sitename### generiert!\nBitte antworten Sie NICHT auf diese E-Mail!\n***********************************\n\nHallo ###mandant_name###,\n",	
 		"xyz.emailmessage.service_new" => "Die externe Dienstleistung -- ###service_name### -- (erstellt durch ###service_community###) wurde bearbeitet.\n",	
 		"xyz.emailmessage.service_deleted" => "Die externe Dienstleistung -- ###service_name### -- (erstellt durch ###service_community###) wurde aus Ihrem Ordner '###external_service_folder###' entfernt. Der Ersteller hat die Dienstleistung entweder gelöscht, oder er leitet sie nicht mehr an Ihre Kommune weiter.\n",	
 		"xyz.emailmessage.service_link" => "Um die Dienstleistung im Auftritt des Erstellers zu sehen, klicken Sie auf nachfolgenden Link: ###service_link###\n\n",
