76f766a9abb5e28b13d25929aff37f6e811fda1d
karina <karina@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-26 11:40:15 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/civserv/trunk@48171 735d13b6-9817-0410-8766-e36946ffe9aa

--- templates_c/%%5D^5DD^5DDA4027%%27866d8ee14858143b653324e757bef4.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2010-07-12 09:35:06
-         compiled from typo3conf/ext/civserv/templates/right_searchbox.tpl.html */ ?>
-<!--<?php if ($this->_tpl_vars['searchbox'] != ""): ?>-->
-	<?php echo $this->_tpl_vars['searchbox']; ?>
-
-<!--<?php endif; ?>-->
